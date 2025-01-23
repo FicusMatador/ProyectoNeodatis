9c0374ad2cecee48d8ae3a5516943d574d3aa588
ErDeivi <dramirezg03@educarex.es>
2025-01-23 19:03:56 +0100

    Delete ProyectoNeodatis directory

--- ProyectoNeodatis/src/main/java/com/example/proyectoneodatis/controladores/InicioControlador.java
+++
@@ -1,5 +0,0 @@
-package com.example.proyectoneodatis.controladores;
-
-public class InicioControlador {
-
-}
--- ProyectoNeodatis/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.proyectoneodatis {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.proyectoneodatis.controladores to javafx.fxml;
-    exports com.example.proyectoneodatis;
-}
