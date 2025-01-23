3a39c0b1c9a66edea8c8b3e45818e24f47651092
David <dramirezg03@educarex.es>
2025-01-23 19:21:47 +0100

    si

--- src/main/java/com/example/proyectoneodatis/controladores/CamposArticulosControlador.java
+++ src/main/java/com/example/proyectoneodatis/controladores/CamposArticulosControlador.java
@@ -1,4 +1,12 @@
 package com.example.proyectoneodatis.controladores;
 
+import javafx.event.ActionEvent;
+
 public class CamposArticulosControlador {
+
+    public void atras(ActionEvent actionEvent) {
+    }
+
+    public void adelante(ActionEvent actionEvent) {
+    }
 }
