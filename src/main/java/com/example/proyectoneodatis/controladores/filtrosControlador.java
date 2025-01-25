31653c6a889d01d12dda663a77fb67c6e2af250b
David <dramirezg03@educarex.es>
2025-01-25 19:33:49 +0100

    cositas

--- src/main/java/com/example/proyectoneodatis/controladores/filtrosControlador.java
+++ src/main/java/com/example/proyectoneodatis/controladores/filtrosControlador.java
@@ -1,4 +1,9 @@
 package com.example.proyectoneodatis.controladores;
 
+import javafx.event.ActionEvent;
+
 public class filtrosControlador {
+    public void buscarOnAction(ActionEvent actionEvent) {
+
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.proyectoneodatis {
     requires javafx.controls;
     requires javafx.fxml;
+    requires neodatis.odb;
 
 
     opens com.example.proyectoneodatis.controladores to javafx.fxml;
