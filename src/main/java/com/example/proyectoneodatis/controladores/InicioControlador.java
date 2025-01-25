275b1b879d096de14e67c16740ae997166e3b687
David <dramirezg03@educarex.es>
2025-01-25 19:52:53 +0100

    a

--- src/main/java/com/example/proyectoneodatis/controladores/InicioControlador.java
+++ src/main/java/com/example/proyectoneodatis/controladores/InicioControlador.java
@@ -1,5 +1,10 @@
 package com.example.proyectoneodatis.controladores;
 
+import javafx.event.ActionEvent;
+
 public class InicioControlador {
 
+    public void inicioSesionOnAction(ActionEvent actionEvent) {
+        
+    }
 }
