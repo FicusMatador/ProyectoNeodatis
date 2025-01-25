e9ba58959663f4c5d784f020adc9440a2a3da245
David <dramirezg03@educarex.es>
2025-01-25 19:58:24 +0100

    cambios chulos

--- src/main/java/com/example/proyectoneodatis/controladores/InicioControlador.java
+++ src/main/java/com/example/proyectoneodatis/controladores/InicioControlador.java
@@ -3,7 +3,7 @@ package com.example.proyectoneodatis.controladores;
 import javafx.event.ActionEvent;
 
 public class InicioControlador {
-    
+
 
     public void inicioSesionOnAction(ActionEvent actionEvent) {
 
