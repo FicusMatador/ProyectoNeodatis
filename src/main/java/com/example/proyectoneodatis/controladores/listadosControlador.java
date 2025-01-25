8c00abb1425c6d340421904c0775388dca6c049e
David <dramirezg03@educarex.es>
2025-01-25 18:15:37 +0100

    Nuevos FXML

--- src/main/java/com/example/proyectoneodatis/controladores/listadosControlador.java
+++ src/main/java/com/example/proyectoneodatis/controladores/listadosControlador.java
@@ -1,4 +1,31 @@
 package com.example.proyectoneodatis.controladores;
 
+import javafx.event.ActionEvent;
+
 public class listadosControlador {
+
+    public void atras(ActionEvent actionEvent) {
+
+    }
+
+    public void buscarOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void nuevoArticuloOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void borrarArticuloOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void modificarOnAction(ActionEvent actionEvent) {
+
+
+    }
+
+    public void exportarOnAction(ActionEvent actionEvent) {
+
+    }
 }
--- src/main/java/com/example/proyectoneodatis/controladores/menuPrincipalControlador.java
+++ src/main/java/com/example/proyectoneodatis/controladores/menuPrincipalControlador.java
@@ -1,4 +1,22 @@
 package com.example.proyectoneodatis.controladores;
 
+import javafx.event.ActionEvent;
+
 public class menuPrincipalControlador {
+    public void gestionarArticulosOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void exportarArticulosOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void importarArticulosOnAction(ActionEvent actionEvent) {
+
+
+    }
+
+    public void cerrarSesionOnAction(ActionEvent actionEvent) {
+
+    }
 }
