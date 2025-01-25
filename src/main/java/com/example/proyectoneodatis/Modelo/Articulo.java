ef2ae8fa2edd7f0f0f36f0c1fcbcf998fd2d1ac7
David <dramirezg03@educarex.es>
2025-01-25 18:35:55 +0100

    s

--- src/main/java/com/example/proyectoneodatis/Modelo/Articulo.java
+++ src/main/java/com/example/proyectoneodatis/Modelo/Articulo.java
@@ -1,4 +1,72 @@
 package com.example.proyectoneodatis.Modelo;
 
 public class Articulo {
+
+    private Integer codigo;
+    private String denominacion;
+    private Double precioDeVentoAlPublico;
+    private String categoria;
+    private Double precioUnitario; // precio unitario
+    private Integer stock;
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDenominacion() {
+        return denominacion;
+    }
+
+    public void setDenominacion(String denominacion) {
+        this.denominacion = denominacion;
+    }
+
+    public Double getPrecioDeVentoAlPublico() {
+        return precioDeVentoAlPublico;
+    }
+
+    public void setPrecioDeVentoAlPublico(Double precioDeVentoAlPublico) {
+        this.precioDeVentoAlPublico = precioDeVentoAlPublico;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public Double getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(Double precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return "Articulo{" +
+                "codigo=" + codigo +
+                ", denominacion='" + denominacion + '\'' +
+                ", precioDeVentoAlPublico=" + precioDeVentoAlPublico +
+                ", categoria='" + categoria + '\'' +
+                ", precioUnitario=" + precioUnitario +
+                ", stock=" + stock +
+                '}';
+    }
+
 }
