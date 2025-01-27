bfd1d5d0fa38c7cfb9256915d9cb638f8a54c29e
JuanColi <juancolillabarragan@gmail.com>
2025-01-27 11:21:04 +0100

    Exportar y Algunos Arreglos

--- src/main/java/com/example/proyectoneodatis/controladores/filtrosControlador.java
+++ src/main/java/com/example/proyectoneodatis/controladores/filtrosControlador.java
@@ -2,7 +2,7 @@ package com.example.proyectoneodatis.controladores;
 
 import javafx.event.ActionEvent;
 
-public class filtrosControlador {
+public class FiltrosControlador {
     public void buscarOnAction(ActionEvent actionEvent) {
 
     }
